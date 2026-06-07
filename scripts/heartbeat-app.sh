8db42788752fe91b363893a92fb8116b4e94f5b4
thanhson208s <thanhson208s@gmail.com>
2026-06-07 13:06:50 +0700

    chore(server): add heartbeat scripts

---
+++ scripts/heartbeat-app.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source /opt/acog/.env.production
+
+curl -fsSL \
+  -H "CF-Access-Client-Id: ${HEARTBEAT_ACCESS_CLIENT_ID}" \
+  -H "CF-Access-Client-Secret: ${HEARTBEAT_ACCESS_CLIENT_SECRET}" \
+  "https://health.acoupleofgamers.com/api/push/KLa09PUFGv?status=up&msg=OK" \
+  >/dev/null
